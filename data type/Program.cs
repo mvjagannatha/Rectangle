d1f4d1749a5647c468a48927039a65615077cf16
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 13:54:51 +0530

    Add project files.

---
+++ data type/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace datatype
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("size of int: {0}", sizeof(int));
+            Console.ReadLine();
+        }
+    }
+}
